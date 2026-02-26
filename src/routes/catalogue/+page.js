73022ef9c794ad7c14bd246078160c09072ad94e
student <darkrevan89@gmail.com>
2026-02-26 14:01:16 +0100

    fix: correction config.js et remplacement localhost dans tous les fichiers

--- src/routes/catalogue/+page.js
+++ src/routes/catalogue/+page.js
@@ -1,9 +1,11 @@
+import { API_URL } from '$lib/config.js';
+
 export async function load({ fetch, url }) {
   const page = url.searchParams.get('page') || '1';
   const limit = '10';
   const search = url.searchParams.get('search') || '';
 
-  let apiUrl = `http://localhost:3000/catalog?page=${page}&limit=${limit}`;
+  let apiUrl = `${API_URL}/catalog?page=${page}&limit=${limit}`;
   if (search) {
     apiUrl += `&search=${encodeURIComponent(search)}`;
   }
